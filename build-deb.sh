e24742b001e002ed801b6f370407eba458e79e9f
Andrey Romanov <ar-webcetera@users.noreply.github.com>
2026-07-02 15:51:47 +0400

    chore: выпустить версию 0.1.11

--- build-deb.sh
+++ build-deb.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 APP=zonerec
-VERSION="${VERSION:-0.1.10}"
+VERSION="${VERSION:-0.1.11}"
 ARCH=all
 ROOT="build/${APP}_${VERSION}_${ARCH}"
 DEB="dist/${APP}_${VERSION}_${ARCH}.deb"
